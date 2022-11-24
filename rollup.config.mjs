9689bab563e1279d3c92485d44ff49efc3b168e5
Gregor Adams <greg@pixelass.com>
2022-11-24 01:02:31 +0100

    chore: bundle adjustment

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,56 +1,24 @@
-import { readdir } from "node:fs/promises";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
 import pkg from "./package.json" assert { type: "json" };
 import { swc } from "rollup-plugin-swc3";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-async function glob(pattern) {
-	try {
-		const files = await readdir(path.resolve(__dirname, pattern), { withFileTypes: true });
-		return (
-			await Promise.all(
-				files.flatMap(async dirent => {
-					if (dirent.isDirectory() && dirent.name !== "__tests__") {
-						return await glob(path.join(pattern, dirent.name));
-					} else if (dirent.isFile() && !dirent.name.includes(".test.")) {
-						return path.join(pattern, dirent.name);
-					}
-					return [];
-				})
-			)
-		).flat();
-	} catch (err) {
-		return [];
-	}
-}
-
-const allFiles = await glob("src");
-
-const config = allFiles.map(filename => {
-	const file = filename.substring(4);
-	const [filePath] = file.split(".");
-	return {
-		external(id) {
-			return [
-				...Object.keys({
-					...(pkg.dependencies ?? {}),
-					...(pkg.peerDependencies ?? {}),
-				}),
-			].some(key => id.startsWith(key) || id.startsWith("."));
-		},
-		input: filename,
+export default [
+	{
+		external: [
+			"react/jsx-runtime",
+			...Object.keys({
+				...(pkg.dependencies ?? {}),
+				...(pkg.peerDependencies ?? {}),
+			}),
+		],
+		input: "src/index.ts",
 		output: [
 			{
-				file: `dist/${filePath}.js`,
+				file: `dist/index.js`,
 				sourcemap: true,
 				format: "cjs",
 			},
 			{
-				file: `dist/${filePath}.mjs`,
+				file: `dist/index.mjs`,
 				sourcemap: true,
 				format: "es",
 			},
@@ -67,7 +35,5 @@ const config = allFiles.map(filename => {
 				},
 			}),
 		],
-	};
-});
-
-export default config;
+	}
+];
